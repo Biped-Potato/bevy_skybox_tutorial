266f3f2b8080d3ea901fd51a106b03d3a55644a2
Platypus88 <79812179+Biped-Potato@users.noreply.github.com>
2024-11-09 13:46:13 -0600

    Final

--- src/camera_controller.rs
+++ src/camera_controller.rs
@@ -1,18 +1,19 @@
-use bevy::{input::mouse::MouseMotion, prelude::*};
+use bevy::prelude::*;
+use bevy::input::mouse::MouseMotion;
 
 #[derive(Component)]
-pub struct CameraController {
-    pub rotation: Vec2,
-    pub rotation_lock: f32,
-    pub sensitivity: f32,
+pub struct CameraController{
+    pub rotation : Vec2,
+    pub rotation_lock : f32,
+    pub sensitivity : f32,
 }
 
 pub fn update_camera_controller(
-    mut mouse_motion: EventReader<MouseMotion>,
-    mut camera_query: Query<(&mut CameraController, &mut Transform)>,
-) {
-    if let Ok((mut camera_controller, mut transform)) = camera_query.get_single_mut() {
-        for ev in mouse_motion.read() {
+    mut mouse_motion : EventReader<MouseMotion>,
+    mut camera_query : Query<(&mut CameraController,&mut Transform)>
+){
+    if let Ok((mut camera_controller,mut transform)) = camera_query.get_single_mut(){
+        for ev in mouse_motion.read(){
             camera_controller.rotation.y -= ev.delta.x * camera_controller.sensitivity;
             camera_controller.rotation.x -= ev.delta.y * camera_controller.sensitivity;
 
@@ -22,8 +23,9 @@ pub fn update_camera_controller(
                 camera_controller.rotation_lock,
             );
         }
+        
         let y_quat = Quat::from_axis_angle(Vec3::Y, camera_controller.rotation.y.to_radians());
         let x_quat = Quat::from_axis_angle(Vec3::X, camera_controller.rotation.x.to_radians());
-        transform.rotation = y_quat * x_quat;
+        transform.rotation = y_quat*x_quat;
     }
 }
